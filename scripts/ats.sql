1273701499fe6365a8924e397c10ca62de1a3eea
chrisccoy <chrisccoy@gmail.com>
2023-02-06 16:40:52 -0700

    Removed both from increase usage

--- scripts/ats.sql
+++ scripts/ats.sql
@@ -9,7 +9,7 @@ select '$NAME' as "name",
 'FALSE' as "isNew",
 '$URL' as "url",
 CASE
-   WHEN EXISTS (select 1 as foo where ((select count(*) from csv."$CSVTABLE" where "$NAME" like '%NEXT%') + (select count(*) from csv."$CSVTABLE" where "$NAME" like '%Both%')) > (select count(*) from csv."$CSVTABLE" where "$NAME" like '%PAST%') ) THEN 1
+   WHEN EXISTS (select 1 as foo where (select count(*) from csv."$CSVTABLE" where "$NAME" like '%NEXT%') > (select count(*) from csv."$CSVTABLE" where "$NAME" like '%PAST%') ) THEN 1
    WHEN EXISTS (select 1 as foo where ((select count(*) from csv."$CSVTABLE" where "$NAME" like '%NEXT%') + 
                 (select count(*) from csv."$CSVTABLE" where "$NAME" like '%Both%')) < (select count(*) from csv."$CSVTABLE" where "$NAME" like '%PAST%') ) THEN -1   ELSE 0
 END as "Direction"
